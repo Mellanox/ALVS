2a7f0cffff580e7249fc630a6ed5f5ec248d75fe
anat zadik <anat@mellanox.com>
2016-03-31 15:09:39 +0300

    update folder structure. create link to EZdk & EZide
    Change-Id: I266b44765c58e22989fc2b830eb82d4f16a8547c
    Signed-off-by: anat zadik <anat@mellanox.com>

--- common/build/common.mk
+++
@@ -1,6 +0,0 @@
-RM := rm -rf
-ENV_BASE := $(DP_BASE)../../..
-
-file := $(ENV_BASE)/tools/sdk/sdk_version.txt
-EZDK_BASE := $(ENV_BASE)/$(shell cat ${file})
-PATH := $(PATH):$(abspath $(EZDK_BASE)/ldk/toolchain/bin)
