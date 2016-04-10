9c5c9fb4d68c44a09ecc2d8692b4591e184cc0b4
Kobi Sagi <kobis@mellanox.com>
2016-04-10 10:15:12 +0300

    Verification/LoadBalancer: add clean files (LB & servers)
    Signed-off-by: Kobi Sagi <kobis@mellanox.com>

---
+++ verification/ALVS/LoadBalancer/lvs_dr_lb_clean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function clean_setup_ipvs()
+{
+    echo "Enaable ipv4 forwarding"
+    echo "1" >/proc/sys/net/ipv4/ip_forward
+
+    # disable ipvsadm
+    echo "check ipvs adminstrator off"
+    chkconfig ipvsadm off
+
+    echo "clear ipvs table"
+    ipvsadm -C
+
+    echo "remove virtual interface configuration"
+    ifconfig eth0:0 down
+}
+
+
+#########################################
+#                                       #
+#              Main                     #
+#                                       #
+#########################################
+trap "exit" INT TERM
+trap "kill 0" EXIT
+clean_setup_ipvs
+echo "All done!"
+
---
+++ verification/ALVS/LoadBalancer/lvs_dr_rs_clean.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function clean_setup_ipvs()
+{
+    echo "Stop HTTP daemon"
+    # ---------------------
+    service httpd stop
+
+    echo "Resore loopback port configuration"
+    # ---------------------
+    ifconfig lo:0 down
+
+    echo "restore ARP configuration"
+    # ------------------------------
+    echo "1" >/proc/sys/net/ipv4/ip_forward
+
+    echo "0" >/proc/sys/net/ipv4/conf/all/arp_ignore
+    echo "0" >/proc/sys/net/ipv4/conf/eth0/arp_ignore
+
+    echo "0" >/proc/sys/net/ipv4/conf/all/arp_announce
+    echo "0" >/proc/sys/net/ipv4/conf/eth0/arp_announce
+    
+    echo "remove index.xml"
+    rm /var/www/html/index.html
+
+}
+
+
+#########################################
+#                                       #
+#              Main                     #
+#                                       #
+#########################################
+trap "exit" INT TERM
+trap "kill 0" EXIT
+echo "Cleaning HTTP server setup for IPVS"
+clean_setup_ipvs
+echo "All done!"
+
+
