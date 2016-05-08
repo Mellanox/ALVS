034393881caa67a105687e0283c2c906d8e562d9
allas <allas@mellanox.com>
2016-05-08 16:20:18 +0300

    after script
    Change-Id: Iff04a1b7d4ba1b4977b8e911b43d2e126e499284

--- src/cp/log.h
+++ src/cp/log.h
@@ -30,16 +30,13 @@
 *	file - log.h
 *	description - contains definitions for log.c
 */
-
 #ifndef LOG_H_
 #define LOG_H_
-
 #include "syslog.h"
-
 #ifndef NDEBUG
-#	define LOG_LEVEL LOG_DEBUG
+#define LOG_LEVEL LOG_DEBUG
 #else
-#	define LOG_LEVEL LOG_INFO
+#define LOG_LEVEL LOG_INFO
 #endif
 
 /*****************************************************************************/
@@ -51,7 +48,9 @@
  */
 #ifndef NDEBUG
 #define write_log(priority, s) \
-	syslog(LOG_MAKEPRI(LOG_USER, priority), "%s [FILE: %s:%d FUNC: %s]", s, __FILE__, __LINE__,  __func__)
+	syslog(LOG_MAKEPRI(LOG_USER, priority), \
+			"%s [FILE: %s:%d FUNC: %s]",\
+			s, __FILE__, __LINE__, __func__)
 #else
 #define write_log(priority, s) \
 	syslog(LOG_MAKEPRI(LOG_USER, priority), "%s", s)
@@ -61,7 +60,8 @@
 /*****************************************************************************/
 /*! \fn void open_log(char *s)
  * \brief Open the connection to syslog utility.
- * \param[in] s  an arbitrary identification string which future syslog invocations will prefix to each message
+ * \param[in] s -  an arbitrary identification string which future syslog
+ *					invocations will prefix to each message
  * \return none.
  */
 void open_log(char *s);
