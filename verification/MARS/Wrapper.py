0c733c49b49bf5b4b6e041a3c3fde2edc2b52c07
Anton <antonh@mellanox.com>
2016-12-15 04:31:17 +0200

    MARS-DDP infrastructure
    Change-Id: I4df5c2117eacc5aa17cdb4b422f69e807d5782dd
    Signed-off-by: Anton <antonh@mellanox.com>

--- verification/MARS/Wrapper.py
+++ verification/MARS/Wrapper.py
@@ -8,18 +8,18 @@ import os
 from reg2_wrapper.test_wrapper.standalone_wrapper import StandaloneWrapper
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-class ALVSWrapper(StandaloneWrapper):
+class Wrapper(StandaloneWrapper):
 
     def get_prog_path(self):
         prog = "test_runner.py"
         return prog
 
     def configure_parser(self):
-        super(ALVSWrapper, self).configure_parser()
+        super(Wrapper, self).configure_parser()
         self.add_cmd_argument('-t',  help='Test to run')
         self.add_cmd_argument('--env',  help='test environment', alias='-e')
         self.add_test_attribute_argument('--topo_file', 'topo_file', separator=' ') 
 
 if __name__ == "__main__":
-    alvs_wrapper = ALVSWrapper("ALVS")
-    alvs_wrapper.execute(sys.argv[1:])
+    wrapper = Wrapper("test_runner")
+    wrapper.execute(sys.argv[1:])
--- verification/MARS/clean_wrapper.py
+++ verification/MARS/clean_wrapper.py
@@ -7,16 +7,16 @@ import os
 # Local modules
 from reg2_wrapper.test_wrapper.standalone_wrapper import StandaloneWrapper
 
-class alvs_clean_wrapper(StandaloneWrapper):
+class clean_wrapper(StandaloneWrapper):
 
     def get_prog_path(self):
-        prog = "alvs_clean.py"
+        prog = "clean.py"
         return prog
 
     def configure_parser(self):
-        super(alvs_clean_wrapper, self).configure_parser()
+        super(clean_wrapper, self).configure_parser()
         self.add_test_attribute_argument('--topo_file', 'topo_file', separator=' ')
 
 if __name__ == "__main__":
-    alvs_wrapper = alvs_clean_wrapper("alvs_clean")
-    alvs_wrapper.execute(sys.argv[1:])
+    wrapper = clean_wrapper("clean")
+    wrapper.execute(sys.argv[1:])
